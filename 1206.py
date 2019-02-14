6fc927843f86f6fb950d2b0251976a7170208876
nnyong <jo.youthful@gmail.com>
2019-02-14 21:05:42 +0900

    d01

---
+++ 1206.py
@@ -0,0 +1,10 @@
+for test_case in range(1,11):
+        a=int(input())
+        n=list(map(int,input().split()))
+
+        cnt=0
+        for i in range(2,len(n)):
+                if n[i]>n[i-1] and n[i]>n[i-2] and n[i]>n[i+1] and n[i]>n[i+2]:
+                        cnt+=n[i]-max(n[i-1],n[i-2],n[i+1],n[i+2])
+        
+        print(f'#{test_case} {cnt}')
--- baby_gin.py
+++ baby_gin.py
@@ -1,4 +1,4 @@
-Data = [2,3,4,7,7,7]
+Data = [1,1,1,2,5,7]
 
 print(Data)
 
@@ -9,11 +9,25 @@ for i in Data:
 
 print(count_array)
 
-for i in range(len(count_array)-1):
-    count_array[i+1]=count_array[i]+count_array[i+1]
+for i in range(len(count_array)):
+    if count_array[i]==3:
+        count_array[i]-=3
+    elif count_array[i]==6:
+        count_array[i]-=6
+
+for i in range(len(count_array)-2):
+    if count_array[i]>=1 and count_array[i+1]>=1 and count_array[i+1]:
+        count_array[i]-=1
+        count_array[i+1]-=1
+        count_array[i+2]-=1
 
 print(count_array)
 
+sum=0
 for i in count_array:
-    if i==3:
+    sum+=i
 
+if sum==0:
+    print('babygin!')
+else:
+    print('not babygin')
