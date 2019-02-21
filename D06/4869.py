553fd52b8de32b1b86f86ea37103d8a5a6dd4427
nnyong <jo.youthful@gmail.com>
2019-02-21 22:20:23 +0900

    D06

--- D06/4869.py
+++ D06/4869.py
@@ -6,9 +6,15 @@ sys.stdin=open('4869.txt','r')
 T=int(input())
 for tc in range(1,T+1):
     n=int(input())
-    print(f'#{tc}')
-    print(n)
 
+    def paper(x):
+        while x>=30:
+            return paper(x-10)+paper(x-20)*2
+        if x==20:
+            return 3
+        elif x==10:
+            return 1
 
+    result=paper(n)
 
-
+    print(f'#{tc} {result}')
