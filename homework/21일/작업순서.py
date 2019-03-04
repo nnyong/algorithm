f847141a62cbc959f4443920c2bffea67286ddd3
nnyong <jo.youthful@gmail.com>
2019-03-04 22:31:06 +0900

    homework

--- homework/21일/작업순서.py
+++ homework/21일/작업순서.py
@@ -1,12 +1,25 @@
 import sys
 sys.stdin=open('작업순서.txt','r')
 
-for tc in range(1,11):
+for tc in range(1,5):
     print(f'#{tc}')
-    v, e = map(int, input().split())
-    print(v)
-    print(e)
+    v, e = list(map(int, input().split()))
     data=list(map(int,input().split()))
     print(data)
-    m = [[0] * (v + 1) for i in range(v + 1)]
-    print(m)
+    myMap=[[0]*(v+1) for i in range(v+1)]
+    node = [0] * (v + 1)
+    for i in range(len(data)//2):
+        y=data[i*2]
+        x=data[i*2+1]
+        node[x]+=1
+        myMap[y][x]=1
+    print(myMap)
+    print(node)
+
+    for i in range(1,v+1):
+        if node[i]==0:
+            begin=i
+            break
+
+    for y in range(len(myMap)):
+        for x in range(len(myMap)):
