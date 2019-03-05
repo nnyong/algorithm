ec8eda58e0e785252688bf9876b0318e5e8d8fff
nnyong <jo.youthful@gmail.com>
2019-03-05 11:58:55 +0900

    D13

--- D13/트리.py
+++ D13/트리.py
@@ -1,2 +1,25 @@
 import sys
-sys.stdin=open('트리.txt','r')
+sys.stdin = open('트리.txt','r')
+
+v = int(input())
+data = list(map(int, input().split()))
+m=[[0]*5 for i in range(v+1)]
+
+visited = [0]*(v+1)
+for i in range(len(data)//2):
+    if i == 0:
+        m[data[0]][4] = 0
+    if not visited[data[i*2]]:
+        m[data[i*2]][0] = data[i*2+1]
+        m[data[i*2+1]][3] = data[i*2]
+        m[data[i*2+1]][4] = m[data[i*2]][4]+1
+        m[data[i*2]][2] += 1
+        visited[data[i*2]] = True
+    else:
+        m[data[i*2]][1] = data[i*2+1]
+        m[data[i * 2 + 1]][3] = data[i * 2]
+        m[data[i * 2 + 1]][4] = m[data[i * 2]][4] + 1
+        m[data[i*2]][2] += 1
+
+for i in range(len(m)):
+    print('{} {}'.format(i, m[i]))
--- homework/21일/작업순서.py
+++ homework/21일/작업순서.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin=open('작업순서.txt','r')
 
 for tc in range(1,5):
-    print(f'#{tc}')
+    print('#{}'.format(tc))
     v, e = list(map(int, input().split()))
     data=list(map(int,input().split()))
     print(data)
@@ -13,13 +13,13 @@ for tc in range(1,5):
         x=data[i*2+1]
         node[x]+=1
         myMap[y][x]=1
-    print(myMap)
+    # print(myMap)
     print(node)
 
     for i in range(1,v+1):
         if node[i]==0:
             begin=i
             break
-
-    for y in range(len(myMap)):
-        for x in range(len(myMap)):
+    #
+    # for y in range(len(myMap)):
+    #     for x in range(len(myMap)):
