9e59f2a152646db1093e058e6a2473f9a5ab0e70
nnyong <jo.youthful@gmail.com>
2019-03-06 08:44:25 +0900

    D13

--- D13/사칙연산유효성검사.py
+++ D13/사칙연산유효성검사.py
@@ -3,4 +3,20 @@ sys.stdin=open('사칙연산.txt','r')
 
 for tc in range(1,11):
     n=int(input())
-    
+    data=[]
+    for i in range(n):
+        data.append(list(input().split()))
+    # print(data)
+
+    for i in range(len(data)):
+        if len(data[0])!=2:
+            data.pop(0)
+    # print(data)
+
+    result=1
+    for i in data:
+        if i[1] in ['*','/','+','-']:
+            result=0
+            break
+
+    print('#{} {}'.format(tc,result))
