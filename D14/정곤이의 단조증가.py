be7d9a7db1b19bb46171085f6d37f5dcfac3aac8
nnyong <jo.youthful@gmail.com>
2019-03-06 21:09:34 +0900

    D14

--- D14/정곤이의 단조증가.py
+++ D14/정곤이의 단조증가.py
@@ -5,16 +5,17 @@ T=int(input())
 for tc in range(1,T+1):
     n=int(input())
     data=list(map(int,input().split()))
-    max_value=0
-    for i in range(n):
+    max_value=-1
+    for i in range(n-1):
         for j in range(i+1,n):
             string=str(data[i]*data[j])
-            # print(string)
+            print(string)
+            cnt=0
             for k in range(len(string)-1):
                 if string[k+1]<string[k]:
-                    continue
-                if int(string)>=max_value:
-                    max_value=int(string)
+                    cnt+=1
+            if int(string)>=max_value and cnt==0:
+                max_value=int(string)
 
     if len(str(max_value))==1:
         print('#{} {}'.format(tc,-1))
