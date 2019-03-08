b05304026e81a9e62c09f44f12391ad47d11ea3d
nnyong <jo.youthful@gmail.com>
2019-03-08 23:30:35 +0900

    homework

--- homework/25일/피자굽기.py
+++ homework/25일/피자굽기.py
@@ -7,17 +7,24 @@ for tc in range(1,T+1):
     size, n=map(int,input().split())
     pizza=list(map(int,input().split()))
 
-    o_front=o_rear=-1
-    p_front = p_rear = -1
+    top=-1
+    rear=len(pizza)-1
+    oven=[]
+    for i in range(size):
+        top+=1
+        oven.append((i+1,pizza[top]))
+    # print(oven)
 
-    oven=[0]*size
+    while len(oven)!=1:
+        # for i in range(len(oven)):
+        p=oven.pop(0)
+        if p[1]//2==0:
+            if top!=rear:
+                top+=1
+                oven.append((top+1,pizza[top]))
+        else:
+            oven.append((p[0],p[1]//2))
+    last_pizza=oven.pop()
+    print('#{} {}'.format(tc,last_pizza[0]))
 
-    print(pizza)
-
-    print(oven)
-
-    for i,c in enumerate(pizza)
-        for j in range(size):
-            o_rear += 1
-            oven[o_rear]=i
 
--- homework/homework.md
+++ homework/homework.md
@@ -481,7 +481,34 @@ homework
 - 피자굽기
 
   ```python
+  # 피자굽기
+  import sys
+  sys.stdin=open('피자굽기.txt','r')
   
+  T=int(input())
+  for tc in range(1,T+1):
+      size, n=map(int,input().split())
+      pizza=list(map(int,input().split()))
+  
+      top=-1
+      rear=len(pizza)-1
+      oven=[]
+      for i in range(size):
+          top+=1
+          oven.append((i+1,pizza[top]))
+      # print(oven)
+  
+      while len(oven)!=1:
+          # for i in range(len(oven)):
+          p=oven.pop(0)
+          if p[1]//2==0:
+              if top!=rear:
+                  top+=1
+                  oven.append((top+1,pizza[top]))
+          else:
+              oven.append((p[0],p[1]//2))
+      last_pizza=oven.pop()
+      print('#{} {}'.format(tc,last_pizza[0]))
   ```
 
   
