3f399b2474a9d4414711f5b40886afb5eea8283a
nnyong <jo.youthful@gmail.com>
2019-03-28 22:54:06 +0900

    D26

--- D26/보급로.py
+++ D26/보급로.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin=open('보급로.txt','r')
 
-dy=[0,1,0,-1]
+dy=[0,1,0,-1]       #우 하 좌 상
 dx=[1,0,-1,0]
 
 def isSafe(x, y):
@@ -10,27 +10,25 @@ def isSafe(x, y):
     else:
         return False
 
-def path(y, x, ans=0):
-    global min_value
-    visited[y][x]=True
-    ans += data[y][x]
-    if ans>min_value:
-        return
+def path(y, x):
     if y==n-1 and x==n-1:
-        min_value=ans
         return
-    for dir in range(4):
-        if isSafe(x+dx[dir],y+dy[dir]) and not visited[y+dy[dir]][x+dx[dir]]:
-            path(y + dy[dir], x + dx[dir],ans)
-            visited[y+dy[dir]][x+dx[dir]]=0
-
+    for direc in range(4):
+        if isSafe(x+dx[direc],y+dy[direc]):
+        # if isSafe(x + dx[direc], y + dy[direc]) and not visited[y + dy[direc]][x + dx[direc]]:
+            if mountain[y][x]+data[y+dy[direc]][x+dx[direc]]<mountain[y+dy[direc]][x+dx[direc]] and mountain[y][x]+data[y+dy[direc]][x+dx[direc]]<mountain[n-1][n-1]:
+                mountain[y+dy[direc]][x+dx[direc]]=mountain[y][x]+data[y+dy[direc]][x+dx[direc]]
+                # visited[y+dy[direc]][x+dx[direc]] = True
+                path(y + dy[direc], x + dx[direc])
+                # visited[y+dy[dir]][x+dx[dir]]=0
 
 T=int(input())
 for tc in range(1,T+1):
     n=int(input())
     data=[list(map(int,input())) for _ in range(n)]
-
-    visited=[[0]*n for _ in range(n)]
-    min_value=987654321
+    mountain=[[987654321]*n for _ in range(n)]
+    # min_value=987654321
+    mountain[0][0]=0
+    # visited=[[0]*n for _ in range(n)]
     path(0,0)
-    print('#{} {}'.format(tc,min_value))
+    print('#{} {}'.format(tc,mountain[n-1][n-1]))
